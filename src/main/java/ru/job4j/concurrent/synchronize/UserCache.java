cda5a1aa3cb274bf1b1b1fda4478c77ab3b94f1d
Aleksey Krylov <elsite2015@yandex.ru>
2021-02-18 15:52:54 +0400

    4. Thread без общих ресурсов [#477112]

---
+++ src/main/java/ru/job4j/concurrent/synchronize/UserCache.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent.synchronize;
+
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> list = new LinkedList<>();
+        for (Integer id : users.keySet()) {
+            list.add(User.of(users.get(id).getName()));
+        }
+        return list;
+    }
+}
