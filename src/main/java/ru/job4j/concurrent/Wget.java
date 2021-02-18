7f0f1299dafb3aa84e5cb0e07814fc054289ae6d
Aleksey Krylov <elsite2015@yandex.ru>
2021-02-18 09:00:18 +0400

    4. Скачивание файла с ограничением. [#477102]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,52 @@
+package ru.job4j.concurrent;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+
+public class Wget implements Runnable {
+    private final String url;
+    private final int speed;
+
+    public Wget(String url, int speed) {
+        this.url = url;
+        this.speed = speed;
+    }
+
+    @Override
+    public void run() {
+        long speedOf1024Byte = 2141;
+        long delay;
+        if (speed > speedOf1024Byte) {
+            delay = (speed - speedOf1024Byte) / 4;
+        } else {
+            delay = 0;
+        }
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream
+                     = new FileOutputStream("src/test/resources/pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                Thread.sleep(delay);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        long timeSTART = System.currentTimeMillis();
+        String url = args[0];
+        int speed = Integer.parseInt(args[1]);
+        Thread wget = new Thread(new Wget(url, speed));
+        wget.start();
+        wget.join();
+        long timeEND = System.currentTimeMillis();
+        System.out.println(timeEND - timeSTART + " milliseconds");
+    }
+}
