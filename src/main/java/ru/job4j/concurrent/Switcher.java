912946145de114fca12c739584a373cd1cd25a05
Aleksey Krylov <elsite2015@yandex.ru>
2021-03-01 12:36:01 +0400

    2. Thread switcher [#477128]

---
+++ src/main/java/ru/job4j/concurrent/Switcher.java
@@ -0,0 +1,50 @@
+package ru.job4j.concurrent;
+
+public class Switcher {
+    private volatile boolean yield = false;
+
+    public void print(String name) {
+        synchronized (this) {
+            notify();
+            System.out.println(name);
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Switcher switcher = new Switcher();
+        Thread first = new Thread(
+                () -> {
+                    while (true) {
+                        switcher.print("Thread A");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                    while (true) {
+                        switcher.print("Thread B");
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+    }
+}
